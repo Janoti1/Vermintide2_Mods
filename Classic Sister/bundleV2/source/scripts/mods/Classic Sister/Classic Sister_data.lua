e9aa2c7ca4f61200af5e2410ecba4c207ca3edd4
Janoti! <35386620+Janoti1@users.noreply.github.com>
2022-11-07 07:12:22 +0100

    Classic Sister Upload

---
+++ Classic Sister/bundleV2/source/scripts/mods/Classic Sister/Classic Sister_data.lua
@@ -0,0 +1,29 @@
+local mod = get_mod("Classic Sister")
+
+return {
+	name = "Classic Sister",
+	description = mod:localize("mod_description"),
+	is_togglable = true,
+	options = {
+		widgets = {
+			{
+				setting_id    = "classic_sister_enable_old_radiant",
+				type          = "checkbox",
+				tooltip       = "classic_sister_enable_old_radiant_tooltip",
+				default_value = false
+			},
+			{
+				setting_id    = "classic_sister_enable_old_moonbow",
+				type          = "checkbox",
+				tooltip       = "classic_sister_enable_old_moonbow_tooltip",
+				default_value = false
+			},
+			{
+				setting_id    = "classic_sister_disable_repel_sound",
+				type          = "checkbox",
+				tooltip       = "classic_sister_disable_repel_sound_tooltip",
+				default_value = false
+			}
+		}
+	}
+}
---
+++ Classic Sister/scripts/mods/Classic Sister/Classic Sister_localization.lua
@@ -0,0 +1,25 @@
+return {
+	mod_description = {
+		en = "Classic Sister",
+	},
+	classic_sister_enable_old_radiant = {
+		en = "Enable Radiant Inheritance on release"
+	},
+	classic_sister_enable_old_radiant_tooltip = {
+		en = "Enable Radiant Inheritance as it was on release."
+	},
+	classic_sister_enable_old_moonbow = {
+		en = "Enable prenerf Moonfirebow"
+	},
+	classic_sister_enable_old_moonbow_tooltip = {
+		en = "Enable prenerf Moonfirebow."
+	},
+	classic_sister_disable_repel_sound = {
+		en = "Disable Repel's sound effect"
+	},
+	classic_sister_disable_repel_sound_tooltip = {
+		en = "Disable Repel's sound effect.\n(restart required to enable the sound effect again)"
+	}
+}
+
+

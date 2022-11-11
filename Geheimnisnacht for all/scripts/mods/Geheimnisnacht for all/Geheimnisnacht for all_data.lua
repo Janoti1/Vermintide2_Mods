1d33f844fd8835a1bccb45b95b3b0fc6c883fb86
Janoti! <35386620+Janoti1@users.noreply.github.com>
2022-11-11 19:15:43 +0100

    Delete Geheimnisnacht for all directory

--- Geheimnisnacht for all/scripts/mods/Geheimnisnacht for all/Geheimnisnacht for all_data.lua
+++
@@ -1,7 +0,0 @@
-local mod = get_mod("Geheimnisnacht for all")
-
-return {
-	name = "Geheimnisnacht+",
-	description = mod:localize("mod_description"),
-	is_togglable = true,
-}
--- Geheimnisnacht for all/scripts/mods/Geheimnisnacht for all/Geheimnisnacht for all_localization.lua
+++
@@ -1,5 +0,0 @@
-return {
-	mod_description = {
-		en = "Spawns the ritual side of the three eyed skull in all adventure maps.",
-	},
-}

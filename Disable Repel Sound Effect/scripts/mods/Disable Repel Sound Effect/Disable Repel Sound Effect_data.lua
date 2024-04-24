03c05123aef967d12c8ec614f15d90aa464e13ea
Janoti! <35386620+Janoti1@users.noreply.github.com>
2024-04-24 22:28:30 +0200

    v1.1
    - The SFX can now be turned on and off without restarting the game

--- Disable Repel Sound Effect/scripts/mods/Disable Repel Sound Effect/Disable Repel Sound Effect_data.lua
+++ Disable Repel Sound Effect/scripts/mods/Disable Repel Sound Effect/Disable Repel Sound Effect_data.lua
@@ -4,4 +4,15 @@ return {
 	name = "Disable Repel Sound Effect",
 	description = mod:localize("mod_description"),
 	is_togglable = false,
+	options = {
+		widgets =
+		{
+			{
+				setting_id    = "disable_repel_sound_effect_id",
+				type          = "checkbox",
+				default_value = true,
+				tooltip = "disable_repel_sound_effect_tooltip",
+			}
+		}
+	}
 }
--- Disable Repel Sound Effect/scripts/mods/Disable Repel Sound Effect/Disable Repel Sound Effect_localization.lua
+++ Disable Repel Sound Effect/scripts/mods/Disable Repel Sound Effect/Disable Repel Sound Effect_localization.lua
@@ -2,4 +2,10 @@ return {
 	mod_description = {
 		en = "Disables the sound effect from sister of the thorns talent Repel",
 	},
+	disable_repel_sound_effect_id = {
+		en = "Disable Repel's sound effect"
+	},
+	disable_repel_sound_effect_tooltip = {
+		en = "Disable Repel's sound effect."
+	}
 }
